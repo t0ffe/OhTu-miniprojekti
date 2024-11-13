6ce17301ec11f9a9f00f6fc298ec4fc8b348c0be
Heidi Tapani <heidi.tapani@helsinki.fi>
2024-11-13 20:05:35 +0200

    Change reference.py Class Reference

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,9 +1,10 @@
 class Reference:
-    def __init__(self, id, content, done):
+    def __init__(self, id, author, title, journal, year):
         self.id = id
-        self.content = content
-        self.done = done
+        self.author = author
+        self.title = title
+        self.journal = journal
+        self.year = year
 
     def __str__(self):
-        is_done = "done" if self.done else "not done"
-        return f"{self.content}, {is_done}"
+        return f"{self.author}. {self.title}. {self.journal}, {self.year}."
