2726f87a043ed8e2df977f2e0bae2d397416cd0f
candyliaa <astronomy.dev@gmail.com>
2024-11-13 21:21:03 +0200

    cleaner code formatting

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -7,22 +7,7 @@ from entities.reference import Reference
 def get_db_contents():
     result = db.session.execute(text(f"SELECT * FROM articles"))
     contents = result.fetchall()
-    return [
-        Reference(
-            row[0],
-            row[1],
-            row[2],
-            row[3],
-            row[4],
-            row[5],
-            row[6],
-            row[7],
-            row[8],
-            row[9],
-            row[10],
-        )
-        for row in contents
-    ]
+    return [Reference(*row) for row in contents]
 
 
 def create_reference(author, title, journal, year):
