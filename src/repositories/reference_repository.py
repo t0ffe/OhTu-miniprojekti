6e1ce2a5ddd40042261761baca917aafa6936c09
candyliaa <astronomy.dev@gmail.com>
2024-11-13 20:58:34 +0200

    get_db_contents() now also gets the optional columns; default NULL

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -7,9 +7,29 @@ from entities.reference import Reference
 def get_db_contents():
     result = db.session.execute(text(f"SELECT * FROM articles"))
     contents = result.fetchall()
-    return [Reference(row[0], row[1], row[2], row[3]) for row in contents]
+    return [
+        Reference(
+            row[0],
+            row[1],
+            row[2],
+            row[3],
+            row[4],
+            row[5],
+            row[6],
+            row[7],
+            row[8],
+            row[9],
+            row[10],
+        )
+        for row in contents
+    ]
+
 
 def create_reference(author, title, journal, year):
-    sql = text("INSERT INTO articles (author, title, journal, year) VALUES (:author, :title, :journal, :year)")
-    db.session.execute(sql, {"author": author, "title": title, "journal": journal, "year": year})
+    sql = text(
+        "INSERT INTO articles (author, title, journal, year) VALUES (:author, :title, :journal, :year)"
+    )
+    db.session.execute(
+        sql, {"author": author, "title": title, "journal": journal, "year": year}
+    )
     db.session.commit()
