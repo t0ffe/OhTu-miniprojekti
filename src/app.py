db772ed7beb768d4970637b7dd13fada157b78f7
candyliaa <astronomy.dev@gmail.com>
2024-11-13 12:53:02 +0200

    db functions in app.py and db_helper.py take a table_name parameter + pylint formatting

--- src/app.py
+++ src/app.py
@@ -4,19 +4,22 @@ from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
 from util import validate_todo
 
+
 @app.route("/")
 def index():
     todos = get_todos()
     unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    return render_template("index.html", todos=todos, unfinished=unfinished)
+
 
 @app.route("/new_todo")
 def new():
     return render_template("new_todo.html")
 
-@app.route("/create_todo", methods=["POST"])
+
+@app.route("/create_reference", methods=["POST"])
 def todo_creation():
-    content = request.form.get("content")
+    content = request.form.get("reference")
 
     try:
         validate_todo(content)
@@ -24,16 +27,20 @@ def todo_creation():
         return redirect("/")
     except Exception as error:
         flash(str(error))
-        return  redirect("/new_todo")
+        return redirect("/new_todo")
+
 
 @app.route("/toggle_todo/<todo_id>", methods=["POST"])
 def toggle_todo(todo_id):
     set_done(todo_id)
     return redirect("/")
 
+
 # testausta varten oleva reitti
 if test_env:
+
     @app.route("/reset_db")
     def reset_database():
-        reset_db()
-        return jsonify({ 'message': "db reset" })
+        table_name = request.form.get("table_name")
+        reset_db(table_name)
+        return jsonify({"message": "db reset"})
--- src/db_helper.py
+++ src/db_helper.py
@@ -1,48 +1,52 @@
 from config import db, app
 from sqlalchemy import text
 
-table_name = "todos"
 
 def table_exists(name):
-  sql_table_existence = text(
-    "SELECT EXISTS ("
-    "  SELECT 1"
-    "  FROM information_schema.tables"
-    f" WHERE table_name = '{name}'"
-    ")"
-  )
+    sql_table_existence = text(
+        "SELECT EXISTS ("
+        "  SELECT 1"
+        "  FROM information_schema.tables"
+        f" WHERE table_name = '{name}'"
+        ")"
+    )
 
-  print(f"Checking if table {name} exists")
-  print(sql_table_existence)
+    print(f"Checking if table {name} exists")
+    print(sql_table_existence)
 
-  result = db.session.execute(sql_table_existence)
-  return result.fetchall()[0][0]
+    result = db.session.execute(sql_table_existence)
+    return result.fetchall()[0][0]
 
-def reset_db():
-  print(f"Clearing contents from table {table_name}")
-  sql = text(f"DELETE FROM {table_name}")
-  db.session.execute(sql)
-  db.session.commit()
 
-def setup_db():
-  if table_exists(table_name):
-    print(f"Table {table_name} exists, dropping")
-    sql = text(f"DROP TABLE {table_name}")
+def reset_db(table_name):
+    print(f"Clearing contents from table {table_name}")
+    sql = text(f"DELETE FROM {table_name}")
     db.session.execute(sql)
     db.session.commit()
 
-  print(f"Creating table {table_name}")
-  sql = text(
-    f"CREATE TABLE {table_name} ("
-    "  id SERIAL PRIMARY KEY, "
-    "  content TEXT NOT NULL,"
-    "  done BOOLEAN DEFAULT FALSE"
-    ")"
-  )
 
-  db.session.execute(sql)
-  db.session.commit()
+def setup_db(table_name):
+    if table_exists(table_name):
+        print(f"Table {table_name} exists, dropping")
+        sql = text(f"DROP TABLE {table_name}")
+        db.session.execute(sql)
+        db.session.commit()
+
+    print(f"Creating table {table_name}")
+    sql = text(
+        f"CREATE TABLE {table_name} ("
+        "  id SERIAL PRIMARY KEY,"
+        "  author TEXT NOT NULL,"
+        "  title TEXT NOT NULL,"
+        "  journal TEXT NOT NULL,"
+        "  year TEXT NOT NULL,"
+        ")"
+    )
+
+    db.session.execute(sql)
+    db.session.commit()
+
 
 if __name__ == "__main__":
     with app.app_context():
-      setup_db()
+        setup_db("articles")
