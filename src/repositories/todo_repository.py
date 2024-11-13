c8b831f90ea0a3426f2edf5819f73354468b219e
Matias Ilola <matias.ilola@helsinki.fi>
2024-11-13 15:23:22 +0200

    set_done poistettu

--- src/repositories/todo_repository.py
+++ src/repositories/todo_repository.py
@@ -9,13 +9,6 @@ def get_db_contents():
     contents = result.fetchall()
     return [Todo(row[0], row[1], row[2], row[3]) for row in contents]
 
-
-def set_done(todo_id):
-    sql = text("UPDATE todos SET done = TRUE WHERE id = :id")
-    db.session.execute(sql, {"id": todo_id})
-    db.session.commit()
-
-
 def create_todo(author, title, journal, year):
     sql = text("INSERT INTO articles (author, title, journal, year) VALUES (:author, :title, :journal, :year)")
     db.session.execute(sql, {"author": author, "title": title, "journal": journal, "year": year})
