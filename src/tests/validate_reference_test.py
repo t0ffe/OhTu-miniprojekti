067c871f58846b009f97def5b479d1186e9252b1
Heidi Tapani <heidi.tapani@helsinki.fi>
2024-11-13 21:02:09 +0200

    Fix tests in validate_reference_test.py and util.py

--- src/tests/validate_reference_test.py
+++ src/tests/validate_reference_test.py
@@ -6,12 +6,12 @@ class TestReferenceValidation(unittest.TestCase):
         pass
 
     def test_valid_length_does_not_raise_error(self):
-        validate_reference("juokse")
-        validate_reference("a" * 100)
+        validate_reference("Author", "Title", "Journal", "2023")
+        validate_reference("j" * 100, "h" * 100, "m" * 100, "2023")
 
     def test_too_short_or_long_raises_error(self):
         with self.assertRaises(UserInputError):
-            validate_reference("ole")
+            validate_reference("", "Title", "Journal", "2023")
 
         with self.assertRaises(UserInputError):
-            validate_reference("koodaa" * 20)
+            validate_reference("koodaa" * 201, "Title", "Journal", "2023")   
--- src/util.py
+++ src/util.py
@@ -2,9 +2,8 @@ class UserInputError(Exception):
     pass
 
 def validate_reference(author, title, journal, year):
-    if not author or title or journal or year:
+    if not author or not title or not journal or not year:
         raise UserInputError("All fields must be filled")
 
-    if len(author) or len(title) or len(journal) or len(year) > 200:
-    
+    if len(author) > 200 or len(title) > 200 or len(journal) > 200 or len(year) > 200:
         raise UserInputError("Reference information length must be smaller than 200")
