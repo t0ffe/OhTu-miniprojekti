c4d91a5bbff538cc1f7f2f7c20d5ceb490a563df
candyliaa <astronomy.dev@gmail.com>
2024-11-14 16:47:54 +0200

    removed view_references endpoint

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
-from repositories.reference_repository import get_db_contents, create_reference, get_all_references
+from repositories.reference_repository import (
+    get_db_contents,
+    create_reference,
+    get_all_references,
+)
 from config import app, test_env
 from util import validate_reference
 
@@ -28,30 +32,27 @@ def reference_creation():
     note = request.form.get("note")
 
     try:
-        validate_reference(author, title, journal, year, volume, number, pages, month, note)
-        create_reference(author, title, journal, year, volume, number, pages, month, note)
+        validate_reference(
+            author, title, journal, year, volume, number, pages, month, note
+        )
+        create_reference(
+            author, title, journal, year, volume, number, pages, month, note
+        )
         return redirect("/")
     except Exception as error:
         flash(str(error))
         return redirect("/new_reference")
 
 
-@app.route("/view_references")
-def view_references():
-    references = get_db_contents()
-    return render_template("view_references.html")
-
-
 @app.route("/toggle_reference/<reference_id>", methods=["POST"])
 def toggle_reference(reference_id):
     return redirect("/")
+
 
 @app.route("/list_references", methods=["GET"])
 def list_references():
     references = get_all_references()
     return render_template("list_references.html", references=references)
-
-
 
 
 # testausta varten oleva reitti
