5c0f52ab0357d6d138bab946507044279885ddbc
Heidi Tapani <heidi.tapani@helsinki.fi>
2024-11-20 15:14:28 +0200

    Modify buttons and overall look of page

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from db_helper import reset_db
 from repositories.reference_repository import (
     create_reference,
     get_all_references,
+    get_reference_by_id
 )
 from config import app, test_env
 from util import validate_reference
