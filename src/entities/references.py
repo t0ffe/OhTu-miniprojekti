34e9387e39aa39db8c47db6ed0c091fcbbbc805a
t0ffe <toffelaakso@gmail.com>
2024-12-03 17:57:01 +0200

    add reference fields quick and dirty #20

---
+++ src/entities/references.py
@@ -0,0 +1,51 @@
+reference_fields = {
+    "article":       { "required": ["author", "title", "journal", "year"], 
+                       "optional": ["volume", "number", "pages", "month", "note"] },
+    "book":          { "required": ["author", "editor", "title", "publisher", "year"],
+                       "optional": ["volume", "number", "pages", "month", "note"] },
+    "booklet":       { "required": ["author", "title", "howpublished", "address", "year"],
+                       "optional": ["editor", "volume", "number", "organization", "month", "note"] },
+    "conference":    { "required": ["author", "title", "booktitle", "year"],
+                       "optional": ["editor", "volume", "number", "pages", "address", "month", "organization", "publisher", "note"] },
+    "inbook":        { "required": ["author", "title", "booktitle", "publisher", "year"],
+                       "optional": ["editor", "volume", "number", "address", "edition", "month", "pages", "note"] },
+    "incollection":  { "required": ["author", "title", "booktitle", "publisher", "year"],
+                       "optional": ["editor", "volume", "number", "pages", "address", "month"] },
+    "inproceedings": { "required": ["author", "title", "booktitle", "year"],
+                       "optional": ["editor", "volume", "number", "pages", "address", "month", "organization", "publisher"] },
+    "manual":        { "required": ["title", "year"],
+                       "optional": ["author", "organization", "address", "edition", "month", "note"] },
+    "mastersthesis": { "required": ["author", "title", "school", "year"],
+                       "optional": ["type", "address", "month", "note"] },
+    "misc":          { "required": [],
+                       "optional": ["author", "title", "howpublished", "month", "year", "note"] },
+    "phdthesis":     { "required": ["author", "title", "school", "year"],
+                       "optional": ["type", "address", "month", "note"] },
+    "proceedings":   { "required": ["title", "year"],
+                       "optional": ["editor", "volume", "number", "series", "address", "month", "publisher"] },
+    "techreport":    { "required": ["author", "title", "institution", "year"],
+                       "optional": ["type", "number", "address", "month", "note"] },
+    "unpublished":   { "required": ["author", "title", "note"],
+                       "optional": ["month", "year"] },
+}
+
+def get_reference_fields(reference_type):
+    if reference_type not in reference_fields:
+        raise ValueError(f"Invalid reference type: {reference_type}")
+    return reference_fields.get(reference_type, {})
+
+if __name__ == "__main__":
+    # TODO delete later
+    # TODO change location of this?
+    
+    # How to use:
+    # from [minne ikinä sit tää laitetaankin].references import get_reference_fields
+    
+    article = get_reference_fields("article")
+    print(f"article fields: \n {article}\n")
+
+    article_required_fields = article["required"]
+    print(f"article required fields: \n {article_required_fields}\n")
+
+    all_book_fields = get_reference_fields("book")["required"] + get_reference_fields("book")["optional"]
+    print(f"book reference's required and optional fields: \n {all_book_fields}\n")
